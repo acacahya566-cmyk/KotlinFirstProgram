36c227b44a1ed0049aca25e1b9f3468457d2b9d7
acacahya566-cmyk <acacahya566@gmail.com>
2025-09-09 12:44:00 +0700

    Commite Whitespace

---
+++ src/05VariabelKotlin02.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamis variabel = " +dynamicVar)
+
+    var staticVar = 34
+    println("Contoh static variabel = " + staticVar)
+}
