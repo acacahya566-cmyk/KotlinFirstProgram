4591b6e5558436af2581861d097ba8022e65a7a0
acacahya566-cmyk <acacahya566@gmail.com>
2025-09-09 12:02:25 +0700

    commit literal

---
+++ src/01Literalkotlin.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var intLiteral =5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var booleanLiteral = true
+    println ("intLiteral =" + intLiteral)
+    println ("doubleLiteral =" + doubleLiteral)
+    println ("stringLiteral =" + stringLiteral)
+    println ("charLiteral =" + charLiteral)
+    println ("booleanLiteral =" + booleanLiteral)
+}
